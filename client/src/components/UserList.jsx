48f4da1460655274bf382ef7186392b6e4d3e82a
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-04 12:39:35 +0200

    added table component

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,9 +1,11 @@
 import Search from "./Search";
+import UserListTable from "./UserListTable";
 
 export default function UserList() {
     return (
         <section className="card users-container">
             <Search />
+            <UserListTable />
       </section>
     )
 }
