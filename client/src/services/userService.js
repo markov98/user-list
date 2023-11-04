09340759affe7c725556cc761227445f6d6aef06
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-04 17:13:27 +0200

    added  userService and userListItem component

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,1 +1,12 @@
-const url = 'http://localhost:3030/jsonstore';
+const url = 'http://localhost:3030/jsonstore/users';
+
+export const getAll = async () => {
+    try {
+        const response = await fetch(url);
+        const result = await response.json();
+
+        return Object.values(result);
+    } catch (err) {
+        console.log(err);
+    }
+}
