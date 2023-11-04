9d86e12696f561fae7aa0e9847a5d4f7d77dd720
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-04 13:51:50 +0200

    added new user btn and pagination component

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,11 +1,14 @@
+import Pagination from "./Pagination";
 import Search from "./Search";
 import UserListTable from "./UserListTable";
 
 export default function UserList() {
     return (
-        <section className="card users-container">
+        <section classNameName="card users-container">
             <Search />
             <UserListTable />
+            <button className="btn-add btn">Add new user</button>
+            <Pagination />
       </section>
     )
 }
