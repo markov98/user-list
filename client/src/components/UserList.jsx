7d29398f06cbfa042bf50ec0dd7f3adf1d5ff9f5
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-05 15:05:57 +0200

    added ability to hid modal

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -7,15 +7,20 @@ import UserListTable from "./UserListTable";
 export default function UserList() {
     const [showCreate, setShowCreate] = useState(null);
 
-    const createUserClickHandler = () => {
-        setShowCreate(<CreateUserModal />);
+    const showUserCreateModal = () => {
+        setShowCreate(<CreateUserModal hideCreate={hideCreateModal} />);
     }
+
+    const hideCreateModal = () => {
+        setShowCreate(null);
+    }
+
 
     return (
         <section className="card users-container">
             <Search />
             <UserListTable />
-            <button className="btn-add btn" onClick={createUserClickHandler}>Add new user</button>
+            <button className="btn-add btn" onClick={showUserCreateModal}>Add new user</button>
             <Pagination />
             {showCreate}
       </section>
