2e7827750212878d5aa11505708021ddcecec47f
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-05 13:48:43 +0200

    add User Button is now functional

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
+import CreateUserModal from "./CreateUserModal";
 import Pagination from "./Pagination";
 import Search from "./Search";
 import UserListTable from "./UserListTable";
 
 export default function UserList() {
+    const [showCreate, setShowCreate] = useState(null);
+
+    const createUserClickHandler = () => {
+        setShowCreate(<CreateUserModal />);
+    }
+
     return (
         <section className="card users-container">
             <Search />
             <UserListTable />
-            <button className="btn-add btn">Add new user</button>
+            <button className="btn-add btn" onClick={createUserClickHandler}>Add new user</button>
             <Pagination />
+            {showCreate}
       </section>
     )
 }
