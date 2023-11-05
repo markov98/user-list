68d2ba61253757f2e693e3a4ca178cc639ba8909
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-05 16:41:36 +0200

    change architecture a bit

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -6,33 +6,11 @@ import UserListTable from "./UserListTable";
 import { create } from "../services/userService";
 
 export default function UserList() {
-    const [showCreate, setShowCreate] = useState(null);
-
-    const showUserCreateModal = () => {
-        setShowCreate(<CreateUserModal hideCreate={hideCreateModal} userCreate={userCreate} />);
-    }
-
-    const hideCreateModal = () => {
-        setShowCreate(null);
-    }
-
-    const userCreate = async (e) => {
-        e.preventDefault();
-
-        const data = Object.fromEntries(new FormData(e.target));
-        const result = await create(data);
-
-        setShowCreate(null);
-    }
-
-
     return (
         <section className="card users-container">
             <Search />
             <UserListTable />
-            <button className="btn-add btn" onClick={showUserCreateModal}>Add new user</button>
             <Pagination />
-            {showCreate}
       </section>
     )
 }
